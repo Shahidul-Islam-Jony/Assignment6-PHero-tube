bd0e762db2bb6e367fec63c40b2730ac6f111393
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-09-01 23:30:09 +0600

    sort by view button completed

--- index.js
+++ index.js
@@ -2,34 +2,30 @@ const phTube = async () => {
     const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
     const data = await response.json();
     const arrObj = data.data;
-    // console.log(arrObj);
     tab(arrObj)
 }
 
+
 const tab = (data) => {
-    // console.log(data);
     const tabField = document.getElementById('tab');
     data.forEach(tab => {
         const div = document.createElement('div');
-        // console.log(tab);
         div.innerHTML = `
-        <button onclick=showCard('${tab.category_id}'); class='btn capitalize active:bg-red-500'>${tab.category}</button>
+        <button onclick=showCard('${tab.category_id}');getId('${tab.category_id}') class='btn capitalize active:bg-red-500'>${tab.category}</button>
     `;
         tabField.appendChild(div);
     })
 }
 
-const showCard = async (category_id = 1000) => {
-    // console.log(category_id);
+const showCard = async (category_id = '1000', sortBtnClicked = false) => {
     const cardContainer = document.getElementById('card-container');
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${category_id}`);
     const data = await response.json();
-    // console.log(data);
-    const categoryData = data.data;
-    // console.log(categoryData);
+    let categoryData = data.data;
 
     cardContainer.textContent = '';
 
+    // no content handler
     const noContent = document.getElementById('no-content');
     if (categoryData.length === 0) {
         noContent.classList.remove('hidden');
@@ -37,21 +33,28 @@ const showCard = async (category_id = 1000) => {
     else {
         noContent.classList.add('hidden');
     }
+
+    // Sort view
+
+    if (sortBtnClicked) {
+        categoryData.sort((a, b) => parseInt(b.others.views) - parseInt(a.others.views));
+        // console.log(categoryData);
+    }
+
+    // content handler
     categoryData.forEach(cardData => {
-        // console.log(cardData);
 
         // Time calculating
         const time = parseInt(cardData.others.posted_date);
         const hour = parseInt(time / 3600);
-        // console.log(hour);
         const min = parseInt((time - (hour * 3600)) / 60);
-        // console.log(min);
+
         // creating card
         const card = document.createElement('div');
         card.innerHTML = `
-        <div class="card bg-base-100 shadow-xl">
+        <div class="card bg-base-100 rounded-lg shadow-xl">
             <div class="relative">
-                <figure><img class="h-72" src="${cardData.thumbnail}" alt="Shoes" /></figure>
+                <figure><img class="h-72 rounded-lg" src="${cardData.thumbnail}" alt="Shoes" /></figure>
                 <p class="absolute right-2 bottom-2 px-2 py-1 rounded-lg text-white bg-black">${time ? `${hour}hrs ${min} min ago` : ''}</p>
             </div>
             <div class="card-body flex flex-row">
@@ -69,13 +72,18 @@ const showCard = async (category_id = 1000) => {
     `;
         cardContainer.appendChild(card);
     })
-
 }
 
 
+const getId=(id='1000')=>{
+    if(document.getElementById('sort-view').addEventListener('click',function(){
+        showCard(id,true);
+        console.log('hi');
+    }));
+      
+}
+getId();
 
-
-/* <p class="absolute right-0 bottom-0 bg-red-400 text-white">${hour ? hour:''}hrs ${min ? min:''} min ago</p> */
 
 showCard();
 phTube();
